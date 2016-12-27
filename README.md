30c67b31ce6b6ded77ac91b323ec2e04e0e225c1
Kevin George <kevin.george@protonmail.com>
2016-12-26 21:36:13 -0600

    Added weather API calls

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Grab_Yo_Umbrella
-## A utility for people too lazy to use a weather app everyday
+# Get Yo Umbrella
+A utility for people too lazy to use a weather app everyday
 
-# Conditions
-## *nix machines only
-## Tested with Python 2.7.x, Having pip helps
-## Alertee resides in the US
-## Know how to setup cron jobs
+## Setup  
 
-# Dependencies
-## PyYAML
-## smptlib
+- Python 2.7.x 
+- Tested on Ubuntu Linux  
+- Dependencies
+  ``` pip install PyYAML
+   pip install smptlib ```
--- alert_me.py
+++ alert_me.py
@@ -1,24 +1,12 @@
+import sys
 import smtplib
 import yaml
+import requests
+import json
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-if __name__ == '__main__':
-
-    #Get the config information about SMTP Server
-    config_file = open('config.yaml')
-    config = yaml.safe_load(config_file)
-    config_file.close()
-
-    msg = MIMEMultipart()
-    msg['Subject'] = config['message']['text']
-    if config['message']['deets'] == 'CHANCE':
-        text = '90% CHANCE'
-        msg.attach(MIMEText(text))
-    elif config['message']['deets'] == 'TEMP':
-        text = '70 degrees'
-        msg.attach(MIMEText(text))
-
+def send_email(config, msg):
     server = smtplib.SMTP(config['smtp']['server'], int(config['smtp']['port']))
     #Identify client to the SMTP server according to RFC 2821
     server.ehlo()
@@ -32,3 +20,37 @@ if __name__ == '__main__':
                     config['message']['alertee'], msg.as_string())
 
     server.quit()
+
+if __name__ == '__main__':
+    #Get the config information
+    config_file = open(sys.argv[1])
+    config = yaml.safe_load(config_file)
+    config_file.close()
+
+    if config['weather']['provider'] == 'openweathermap':
+        request_url = "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" \
+        + config['weather']['app_id'] \
+        + "&id=" + str(config['weather']['city_id']) \
+        + "&cnt=" + str(config['weather']['days']) \
+        + "&units=" + config['weather']['units'] \
+        + "&lang=" + config['weather']['language']
+    else:
+        print "Please specify weather provider in config.yaml"
+
+    req = requests.get(request_url)
+    msg = MIMEMultipart()
+    if req.status_code != 200:
+        msg['Subject'] = "FAILURE!"
+        text = "Weather API failed"
+        msg.attach(MIMEText(text))
+        send_email(config, msg)
+    else:
+        content = json.loads(req.text)
+        forecast = content['list'][0]
+        condition = forecast['weather'][0]
+        #Only send email if it's gonna rain
+        if condition['main'] == 'Rain':
+            msg['Subject'] = config['message']['text']
+            text = condition['description']
+            msg.attach(MIMEText(text))
+            send_email(config, msg)
